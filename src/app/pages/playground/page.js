5bc6522c0fd5788171b75a57ab67b0b05cff3a16
Yeji Seo <faithyejiseo@gmail.com>
2024-12-21 04:51:41 -0500

    Playground photos/videos added!

--- src/app/pages/playground/page.js
+++ src/app/pages/playground/page.js
@@ -1,9 +1,193 @@
 import Image from "next/image";
+import "./playground.css";
+
 export default function Playground() {
   return (
     <main className="overflow-hidden">
-      <div className="flex flex-col mt-60 justify-center items-center text-center gap-2 text-neutral-600">
-        Playground!!
+      <h1 className="text-serif italic text-neutral-600 m-auto text-center mt-6 text-2xl">
+        My collection of creative experiments!
+      </h1>
+      <div className="container">
+        <div className="box">
+          <img src="/images/playground/drawings/doodling.webp" alt="Doodling" />
+        </div>
+        <div className="box">
+          <video
+            src="/images/playground/experiments/AllCardsVideo.webm"
+            autoPlay
+            loop
+            muted
+            className="w-full"
+          ></video>
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/drawings/childsplay.webp"
+            alt="Child's Play"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/brand/ebaesTablemat.webp"
+            alt="Ebae's Tablemat"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/brand/GBFlogos.webp" alt="GBF Logos" />
+        </div>
+        <div className="box">
+          <img src="/images/playground/graphics/colorful.webp" alt="Colorful" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/brand/plantasiaLogos.webp"
+            alt="Plantasia Logos"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/activist2.webp"
+            alt="Activist 2"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/drawings/JoseonMetro.webp"
+            alt="Joseon Metro"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/experiments/deskDumpster.webp"
+            alt="Desk Dumpster"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/brand/grocerase.webp" alt="Grocerase" />
+        </div>
+        <div className="box">
+          <video
+            src="/images/playground/experiments/CUGirl.webm"
+            autoPlay
+            loop
+            muted
+            className="w-full"
+          ></video>
+        </div>
+        <div className="box">
+          <img src="/images/playground/drawings/crayons.webp" alt="Crayons" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/ConquestMap.webp"
+            alt="Conquest Map"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/drawings/theMan.webp" alt="The Man" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/drawings/botstancomp.webp"
+            alt="Botstan Comp"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/experiments/ascii.webp" alt="ASCII" />
+        </div>
+        <div className="box">
+          <img src="/images/playground/experiments/lamp.webp" alt="Lamp" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/experiments/domoscene.webp"
+            alt="Domo Scene"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/drawings/alien.webp" alt="Alien" />
+        </div>
+        <div className="box">
+          <img src="/images/playground/experiments/jeong.webp" alt="Jeong" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/experiments/woodenBox.webp"
+            alt="Wooden Box"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/experiments/YEJI.webp" alt="YEJI" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/experiments/tableclothShirt.webp"
+            alt="Tablecloth Shirt"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/graphics/welcome.webp" alt="Welcome" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead2.webp"
+            alt="Fathead 2"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/graphics/activist.webp" alt="Activist" />
+        </div>
+        <div className="box">
+          <img src="/images/playground/graphics/deck.webp" alt="Deck" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/ebaesFlyer.webp"
+            alt="Ebae's Flyer"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead6.webp"
+            alt="Fathead 6"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/brand/treffa.webp" alt="Treffa" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead5.webp"
+            alt="Fathead 5"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead3.webp"
+            alt="Fathead 3"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead4.webp"
+            alt="Fathead 4"
+          />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/experiments/spoonButler.webp"
+            alt="Spoon Butler"
+          />
+        </div>
+        <div className="box">
+          <img src="/images/playground/graphics/totoro.webp" alt="Totoro" />
+        </div>
+        <div className="box">
+          <img
+            src="/images/playground/graphics/fathead1.webp"
+            alt="Fathead 1"
+          />
+        </div>
       </div>
     </main>
   );
